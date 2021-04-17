dd7f5745d4f7d4cb3ead356672e05b0ef8572901
Snoopydodo <69661632+snoopydodo@users.noreply.github.com>
2021-04-18 01:02:03 +0700

    Added Bkk of Ecxotic Pet location

--- lib/bkkexotic.dart
+++ lib/bkkexotic.dart
@@ -88,8 +88,43 @@ class HomePageState extends State<BkkExotic> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: _boxes(
-                  "https://www.halalroute.in.th/wp-content/uploads/2016/10/IMG_1123.jpg",
-                  13.7844286,100.3936433,"Cat In Cafe"),
+                  "https://www.misstamchiak.com/wp-content/uploads/2018/03/animal-cafe-featured--1300x929.jpg",
+                  13.6998991,100.5319989,"The Animal Cafe"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://www.ryoiireview.com/upload/article/201705/1496048643_a5aeda6d320d66642f1bae7f53a0091c.jpg",
+                  13.7746503,100.6648727,"Pet Public Cafe"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://images.squarespace-cdn.com/content/v1/5f1a5f45ca5a6f1c30bd3aac/1596293700724-4L05T4SI1U3IBQ0MRS9R/ke17ZwdGBToddI8pDm48kFWxnDtCdRm2WA9rXcwtIYR7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UcTSrQkGwCGRqSxozz07hWZrYGYYH8sg4qn8Lpf9k1pYMHPsat2_S1jaQY3SwdyaXg/IMG_4005.jpeg",
+                  13.7204748,100.5480171,"S'uvimol Tea House"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://img.wongnai.com/p/1920x0/2018/04/07/261f7dd6b15847bfac484e8291315b64.jpg",
+                  13.7589558,100.6749244,"Bunny Sweet Home Cafe"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://media-cdn.tripadvisor.com/media/photo-s/0d/62/77/bc/caption.jpg",
+                  13.6846412,100.6101744,"Rabito Cafe"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://ed.edtfiles-media.com/static-cache/resize-cache/790x528/ud/images/1/147/440324/IMG_5991-20160107.jpg",
+                  13.7278164,100.5351865,"Little Zoo Cafe"),
             ),
             
           ],
@@ -255,12 +290,56 @@ class HomePageState extends State<BkkExotic> {
   }
 }
 
-Marker catInMarker = Marker(
-  markerId: MarkerId('Cat In Cafe'),
-  position: LatLng(13.7844286,100.3936433),
-  infoWindow: InfoWindow(title: 'Cat In Cafe'),
+Marker animalExoticMarker = Marker(
+  markerId: MarkerId('The Animal Cafe'),
+  position: LatLng(13.6998991,100.5319989),
+  infoWindow: InfoWindow(title: 'The Animal Cafe'),
   icon: BitmapDescriptor.defaultMarkerWithHue(
     BitmapDescriptor.hueViolet,
   ),
 );
 
+Marker petPublicMarker = Marker(
+  markerId: MarkerId('Pet Public Cafe'),
+  position: LatLng(13.7746503,100.6648727),
+  infoWindow: InfoWindow(title: 'Pet Public Cafe'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+Marker suvimolTeaMarker = Marker(
+  markerId: MarkerId('Suvimol Tea House'),
+  position: LatLng(13.7204748,100.5480171),
+  infoWindow: InfoWindow(title: 'Suvimol Tea House'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+Marker bunnySweetMarker = Marker(
+  markerId: MarkerId('Bunny Sweet Home Cafe'),
+  position: LatLng(13.7589558,100.6749244),
+  infoWindow: InfoWindow(title: 'Bunny Sweet Home Cafe'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+Marker rabitoExoticMarker = Marker(
+  markerId: MarkerId('Bunny Sweet Home Cafe'),
+  position: LatLng(13.7589558,100.6749244),
+  infoWindow: InfoWindow(title: 'Bunny Sweet Home Cafe'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
+
+Marker littleZooMarker = Marker(
+  markerId: MarkerId('Little Zoo Cafe'),
+  position: LatLng(13.7278164,100.5351865),
+  infoWindow: InfoWindow(title: 'Little Zoo Cafe'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueViolet,
+  ),
+);
